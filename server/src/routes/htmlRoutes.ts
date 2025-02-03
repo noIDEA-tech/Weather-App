36260a9a55c5d63642e85ed7351ebc30a3bb2be4
Nancy Watreas <nwatreas2023@gmail.com>
2025-02-03 12:43:21 -0600

    refactor: update package.json scripts and improve route handler

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
-router.get('*', (req, res) => {
+router.get('*', (_req, res) => {
     if (process.env.NODE_ENV === 'production') {
       res.sendFile(path.join(__dirname, '../../../client/dist/index.html')); //VERIFY ROUTE PATH TO index.html!!
     } else {
